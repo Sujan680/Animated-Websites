800e50bb89106b816141bc569ff47e6cecb4361a
sujan <budhasujan3@gmail.com>
2024-07-13 20:54:16 +0545

    Animated Website using HTML CSS JS day18

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ gsap.to("#nav", {
     scrollTrigger : {
         trigger: "#nav",
         scroller: "body",
-        markers: true,
+        // markers: true,
         start: "top -10%",
         end: "top -11%",
         scrub: 1
@@ -42,7 +42,7 @@ gsap.to("#main", {
     scrollTrigger : {
         trigger: "#main",
         scroller: "body",
-        markers: true,
+        // markers: true,
         start: "top -25%",
         end: "top -70%",
         scrub: 2
@@ -56,9 +56,65 @@ gsap.from("#about-using, #about-us-in",{
     scrollTrigger: {
         trigger: "#about-us",
         scroller: "body",
-        markers: true,
+        // markers: true,
         start:"top 60%",
         end: "top 58%",
+        scrub: 3
+    }
+
+})
+
+gsap.from(".card",{
+    scale: 0.8,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".card",
+        scroller: "body",
+        // markers: true,
+        start:"top 70%",
+        end: "top 65%",
+        scrub: 3
+    }
+})
+
+gsap.from("#col1",{
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+        trigger: "#col1",
+        scroller: "body",
+        // markers: true,
+        start:"top 60%",
+        end: "top 40%",
+        scrub: 4
+    }
+
+})
+
+gsap.from("#col2",{
+    y: 70,
+    x: 70,
+    scrollTrigger: {
+        trigger: "#col1",
+        scroller: "body",
+        // markers: true,
+        start:"top 60%",
+        end: "top 40%",
+        scrub: 4
+    }
+
+})
+
+gsap.from("#page4 h1",{
+    y: 50,
+  
+    scrollTrigger: {
+        trigger: "#page4 h1",
+        scroller: "body",
+        // markers: true,
+        start:"top 75%",
+        end: "top 70%",
         scrub: 3
     }
 
